e65effd4e8f3e66809a95c09c5663e1b710f7d48
Grace Wangui <wanguigrace280@gmail.com>
2022-08-28 20:03:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  reverse the input string
+if the reversed string is the same as the input
+  return true
+else
+  return false
 */
 
 /*
   Add written explanation of your solution here
+  I need to write code to reverse the input string first then
+   I need a code that with the input string that returns true when 
+  the string reads the same whether forwards or backwards otherwise returns 
+  false
 */
 
 // You can run `node index.js` to view these console logs
